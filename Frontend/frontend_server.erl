4d0b04d0e0f67f0dfd42f365022b144aec230bf9
João <jpsilva9898@gmail.com>
2020-01-11 19:05:34 +0000

    Frontend Authentication (#15)
    * Add module protocole.erl generated by the existing dependency (gpb)
    
    * Add enum Type to protocol specifying the message type
    
    * Add a state to protocol in order to get feedback from the operations result
    
    * Upgrade makefile for more features
    
    * Starting version of the frontend server
    
    This version contains login and register features, as well as
    code modularity.

---
+++ Frontend/frontend_server.erl
@@ -0,0 +1,15 @@
+-module(frontend_server).
+-export ([server/1]).
+-include("authentication_handler.erl").
+
+% fuction for starts running the frontend server with somo configurations
+server(Port) ->
+    {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0}, {reuseaddr, true}, {active, true}]),
+    acceptor(LSock).
+
+% function for create an ErlangProcess per client and keep accepting new connections
+acceptor(LSock) ->
+    {ok, Sock} = gen_tcp:accept(LSock),
+    spawn(fun() -> acceptor(LSock) end),
+    gen_tcp:controlling_process(Sock, self()),
+    authenticationHandler(Sock).
