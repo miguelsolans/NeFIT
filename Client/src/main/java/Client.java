350b0d49ce43be7c14ea403b01cbe56876178061
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-01-16 01:23:45 +0000

    Specific menus added (no logic)

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -11,15 +11,54 @@ public class Client implements Runnable {
     private BufferedReader sin;
 
     private String type; // Producer, Consumer
+    private String username;
 
     Client(Socket cs) throws IOException {
         this.cs = cs;
         this.sm = new SocketManager(this.cs);
         this.sin = new BufferedReader(new InputStreamReader(System.in));
+
         this.type = "";
+        this.username = "";
     }
 
-    // check if User exists
+    // INITIAL MENU INTERFACE WITH OPTIONS
+    private boolean initialMenu() throws IOException {
+        System.out.println("------------ NeFIT ------------");
+        while(true) {
+            System.out.println("\n1) Authentication \n2) Registration \n3) Exit");
+            System.out.print("-> ");
+
+            switch (this.sin.readLine()){
+                case "1":
+                    if (!authentication())
+                        System.out.println("\nWRONG CREDENTIALS. TRY AGAIN.\n");
+                    else {
+                        System.out.println("\nACCESS GRANTED.\n");
+                        return true;
+                    }
+                    break;
+
+                case "2":
+                    if (!registration())
+                        System.out.println("\nREGISTRATION ERROR.\n");
+                    else {
+                        System.out.println("\nREGISTRATION SUCCESSFULLY DONE.\n");
+                        return true;
+                    }
+                    break;
+
+                case "3":
+                    return false;
+
+                default:
+                    System.out.println("\nOPTION NOT VALID.\n");
+                    break;
+            }
+        }
+    }
+
+    // LOGIN MENU INTERFACE
     private boolean authentication() throws IOException{
         System.out.print("\nAuthentication\nUsername: ");
         String username = this.sin.readLine();
@@ -51,11 +90,13 @@ public class Client implements Runnable {
                 res.hasUserType()
         ) {
             this.type = res.getUserType();
+            this.username = username;
             return true;
         }
         return false;
     }
 
+    // REGISTER MENU INTERFACE
     private boolean registration() throws IOException{
         System.out.print("\nRegistration\nUsername: ");
         String username = this.sin.readLine();
@@ -102,48 +143,97 @@ public class Client implements Runnable {
         return false;
     }
 
-    private boolean initialMenu() throws IOException {
+    // IMPORTER MENU INTERFACE
+    private boolean importerMenu() throws IOException {
+        System.out.println("Welcome back "+username+"! You logged in as an importer.");
         while(true) {
-            System.out.println("\n1) Authentication \n2) Registration \n3) Exit");
+            System.out.println("\n1) View available offers \n2) New order \n3) Logout");
             System.out.print("-> ");
 
             switch (this.sin.readLine()){
                 case "1":
-                    if (!authentication())
-                        System.out.println("\nWRONG CREDENTIALS. TRY AGAIN.\n");
+                    if (!viewOffers())
+                        System.out.println("\nSOMETHING WENT WRONG WHILE FETCHING THE OFFERS\n");
                     else {
-                        System.out.println("\nACCESS GRANTED.\n");
+                        System.out.println("\nAVAILABLE ORDERS SUCCESSFULLY RETRIEVED.\n");
                         return true;
                     }
                     break;
 
                 case "2":
-                    if (!registration())
-                        System.out.println("\nREGISTRATION ERROR.\n");
-                    else System.out.println("\nREGISTRATION SUCCESSFULLY DONE.\n");
+                    if (!newOrder())
+                        System.out.println("\nSOMETHING WENT WRONG WITH YOUR ORDER\n");
+                    else {
+                        System.out.println("\nORDER SUCCESSFULLY POSTED.\n");
+                        return true;
+                    }
                     break;
 
                 case "3":
                     return false;
 
                 default:
-                    System.out.println("\nOPTION NOT VALID.\n");
+                    System.out.println("\nPLEASE SELECT A VALID OPTION.\n");
                     break;
             }
         }
     }
 
+    // MANUFACTURER MENU INTERFACE
+    private boolean manufacturerMenu() throws IOException {
+        System.out.println("Welcome back "+username+"! You logged in as a manufacturer.");
+        while(true) {
+            System.out.println("\n1) New offer \n2) Logout");
+            System.out.print("-> ");
+
+            switch (this.sin.readLine()){
+                case "1":
+                    if (!newOffer())
+                        System.out.println("\nSOMETHING WENT WRONG WITH YOUR OFFER\n");
+                    else {
+                        System.out.println("\nOFFER SUCCESSFULLY PUBLISHED.\n");
+                        return true;
+                    }
+                    break;
+
+                case "2":
+                    return false;
+
+                default:
+                    System.out.println("\nPLEASE SELECT A VALID OPTION.\n");
+                    break;
+            }
+        }
+    }
+
+    private boolean viewOffers() {
+        return true;
+    }
+
+    private boolean newOrder() {
+        return true;
+    }
+
+    private boolean newOffer() {
+        return true;
+    }
+
+    // RUN THE CLIENT
     public void run(){
         try {
             while(true) {
                 try {
                     if (!initialMenu()) break;
+
+                    if (type.equals("MANUFACTURER")){
+                        if (!manufacturerMenu()) break;
+                    }
+                    else if (type.equals("IMPORTER")) {
+                        if (!importerMenu()) break;
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                // do stuff
-                System.out.println("Passei o menu inicial");
-                break;
             }
 
         } finally {
@@ -154,5 +244,4 @@ public class Client implements Runnable {
             }
         }
     }
-
 }
