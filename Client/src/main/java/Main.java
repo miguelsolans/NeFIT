62aba8efb4b6fd5136fecad95c15ba7c6130332f
hpereira98 <pereirapalmeirahenrique@gmail.com>
2020-01-18 23:14:18 +0100

    Logic changed for subscriptions and some issues fixed

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -5,8 +5,9 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws IOException, UnknownHostException {
         Socket cs = new Socket("127.0.0.1", 9999);
+        int subPort = 1234; // modificar para porta de subscrições dps do ZeroMQ
 
-        Thread mt = new Thread(new Client(cs));
+        Thread mt = new Thread(new Client(cs,subPort));
         mt.start();
 
         try{
