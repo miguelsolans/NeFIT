e1dab53b6cf16634bcf60ded00058ec973cf812c
hpereira98 <pereirapalmeirahenrique@gmail.com>
2020-01-19 21:43:06 +0100

    porta fixed

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws Exception {
         Socket cs = new Socket("127.0.0.1", 9999);
-        int subPort = 1234; // modificar para porta de subscrições dps do ZeroMQ
+        int subPort = 6666; // modificar para porta de subscrições dps do ZeroMQ
 
         Thread mt = new Thread(new Client(cs,subPort));
         mt.start();
