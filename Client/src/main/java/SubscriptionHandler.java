b33e52a62af8e4230c784fab67ab82c46bc015db
hpereira98 <pereirapalmeirahenrique@gmail.com>
2020-01-19 22:47:31 +0100

    Notifications toggling fixed

--- Client/src/main/java/SubscriptionHandler.java
+++ Client/src/main/java/SubscriptionHandler.java
@@ -2,9 +2,14 @@ import org.zeromq.ZMQ;
 
 public class SubscriptionHandler implements Runnable {
     private ZMQ.Socket sub;
+    private boolean on;
 
     SubscriptionHandler(ZMQ.Socket sub) {
         this.sub = sub;
+    }
+
+    void setOn(boolean value) {
+        on = value;
     }
 
     @Override
@@ -13,7 +18,7 @@ public class SubscriptionHandler implements Runnable {
         while (true) {
             byte[] b = sub.recv();
             msg = new String(b);
-            System.out.println("NEW NOTIFICATION: " +msg);
+            if (on) System.out.println("NEW NOTIFICATION: " +msg);
         }
     }
 }
